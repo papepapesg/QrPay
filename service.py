b6f73e42da6ccb998051a0d51445dc617c5fdc78
papepapesg <33980238+papepapesg@users.noreply.github.com>
2022-12-25 13:28:59 +0000

    Update service.py

--- service.py
+++ service.py
@@ -26,22 +26,3 @@ def generate_qr_code():
 
 if __name__ == '__main__':
     app.run()
-To test the API, you can use a tool like cURL or Postman to send a POST request to the /generate-qr-code endpoint with the following form data:
-
-Copy code
-merchantID: 12345
-trxID: 67890
-products[0][name]: Product 1
-products[0][price]: 10.00
-products[1][name]: Product 2
-products[1][price]: 5.00
-This will generate a QR code image with the following data encoded:
-
-Copy code
-Merchant ID: 12345
-Transaction ID: 67890
-Product 1: 10.00
-Product 2: 5.00
-
-
-
